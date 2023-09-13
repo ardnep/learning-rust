27053a5a41356a3b501f3e8cf9372e9720b6c4dd
ardnep <aranepal@gmail.com>
2023-09-14 00:00:04 +0800

    hello_cargo post build

---
+++ hello_cargo/target/debug/deps/hello_cargo-b771045862463e43.d
@@ -0,0 +1,5 @@
+/Users/ardnep/synched-projects/learning/rust/hello_cargo/target/debug/deps/hello_cargo-b771045862463e43.rmeta: src/main.rs
+
+/Users/ardnep/synched-projects/learning/rust/hello_cargo/target/debug/deps/hello_cargo-b771045862463e43.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/ardnep/synched-projects/learning/rust/hello_cargo/target/debug/hello_cargo: /Users/ardnep/synched-projects/learning/rust/hello_cargo/src/main.rs
